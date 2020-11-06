f0157d7a797d6d19b24cf297ee7bad31935c713c
AleksanderSkubala <alexskubala@gmail.com>
2020-11-06 18:53:15 +0100

    complete welcome section

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,58 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sunt dolores consequatur veritatis? Aliquid aspernatur placeat iure veritatis. Quo deleniti vero soluta similique laborum quaerat accusantium distinctio doloribus quos nemo?</p>
+    <WelcomeSection/>
   </div>
 </template>
 
 <script>
+import WelcomeSection from './components/WelcomeSection.vue';
+
 export default {
   name: 'App',
+  components: {
+    WelcomeSection,
+  },
 };
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
-body, html {
-  margin: 0;
-  width: 100vw;
-  min-height: 100vh;
-  overflow-x: hidden;
-  font-family: 'Noto Sans', sans-serif;
-  font-size: 17px;
-  line-height: 1.55;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+  body, html {
+    margin: 0;
+    width: 100vw;
+    min-height: 100vh;
+    overflow-x: hidden;
+    font-family: 'Noto Sans', sans-serif;
+    font-size: 17px;
+    line-height: 1.55;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-*, *::before, *::after {
-  box-sizing: border-box;
-}
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
 
-h1, h2, h3, h4, h5, h6 {
-  font-family: 'Poppins', sans-serif;
-}
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Poppins', sans-serif;
+  }
 
-h1 {
-  font-size: 1.8rem;
-}
+  h1 {
+    font-size: 1.8rem;
+  }
 
-h2 {
-  font-size: 1.3rem;
-}
+  h2 {
+    font-size: 1.3rem;
+  }
+
+  img {
+    height: auto;
+    display: block;
+  }
+
+  .container {
+    width: 100vw;
+    margin-top: 10vw;
+  }
 </style>
---
+++ src/components/WelcomeSection.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="intro">
+    <div class="intro__info">
+      <h1>
+        Stwórz<br/>
+        Swoje<br/>
+        Portfolio<br/>
+      </h1>
+    </div>
+    <Illustration class="intro__illustration"/>
+  </div>
+</template>
+
+<script>
+import Illustration from '../assets/graphic.svg';
+
+export default {
+  name: 'WelcomeSection',
+  components: {
+    Illustration,
+  },
+};
+</script>
+
+<style lang="scss">
+  .intro {
+    width: 100vw;
+    display: flex;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px 80px;
+
+    &::after {
+      content: '';
+      width: 180px;
+      height: 180px;
+      border: 10px dashed #ededed;
+      border-radius: 250px;
+      position: absolute;
+      z-index: -1;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      margin: auto auto;
+    }
+
+    @media (min-width: 768px) {
+      display: grid;
+      grid-template-columns: 0.9fr 1fr;
+      height: 100vh;
+    }
+  }
+
+  .intro__info {
+    text-align: center;
+    height: 50vh;
+
+    @media (min-width: 768px) {
+      padding-right: 30px;
+      text-align: right;
+      height: unset;
+    }
+  }
+
+  .intro__illustration {
+    width: 145%;
+    height: auto;
+
+    @media (min-width: 768px) {
+      width: 110%;
+    }
+  }
+</style>
